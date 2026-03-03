cb7282c39b4b0d99736e3a533cc13dfaa77eb174
Aditya101415 <official.aditya8787@gmail.com>
2026-03-03 23:59:39 +0530

    ..

--- Basics/countDigit.java
+++ Basics/countDigit.java
@@ -5,13 +5,11 @@ public class countDigit{
         int n=sc.nextInt();
         int numofDigit=0;
         int temp=n;
-
         while(n>0){
             n=n/10;
             numofDigit++;
         }
         System.out.println("Number of digits in" + " "+ temp +" "+ "is" +" "+ numofDigit);
-
         sc.close();
     }
 }
