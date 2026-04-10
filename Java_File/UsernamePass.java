b420a59dcc0c80084a4f6c1e61d1f9626b793c2a
Aditya101415 <official.aditya8787@gmail.com>
2026-04-10 21:33:46 +0530

    usenamepass

---
+++ Java_File/UsernamePass.java
@@ -0,0 +1,22 @@
+import java.io.Console;
+
+public class UsernamePass {
+ public static void main(String[] args) {
+
+ Console console = System.console();
+
+ if (console == null) {
+ System.out.println("Console not available. Run this program in terminal.");
+return;
+ }
+
+ String username = console.readLine("Enter Username: ");
+
+ char[] passwordArray = console.readPassword("Enter Password: ");
+ String password = new String(passwordArray);
+
+ System.out.println("\nEntered Username: " + username);
+ System.out.println("Entered Password: " + password);
+ }
+}
+
